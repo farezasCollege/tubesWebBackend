d1144fc7f4b37b73d8787866c279027cc5584728
nisarahma <nisarahma128@yahoo.com>
2019-04-20 14:32:58 +0700

    view update

---
+++ view/Footer.php
@@ -0,0 +1,9 @@
+    </div>
+    <!-- JS -->
+    <script src="<?php echo base_url('Assets/Landing/vendor/jquery/jquery.min.js'); ?>"></script>
+    <script src="<?php echo base_url('Assets/Landing/js/main.js'); ?>"></script>
+
+    <!-- Bootstrap JS -->
+    <script src="<?php echo base_url('Assets/Bootstrap/js/bootstrap.bundle.js'); ?>"></script>
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</html>
